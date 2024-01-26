bdb4057c4fd7bab1c9d7215b7d136c89053af29d
Stones49 <140336580+Stones49@users.noreply.github.com>
2024-01-26 01:09:53 +0100

    Update Config.js

--- Config.js
+++ Config.js
@@ -2,18 +2,18 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernumber = ['919931122319']
-global.ownername = "💙𓆩‎✘AʏᴜSʜ✘𓆪_✰"//owner name
+global.ownernumber = ['09163418932']
+global.ownername = "Roy
 global.ytname = "YT: AyushBotz.inc"
 global.socialm = "GitHub: Ayushpandey023"
-global.location = "Indian"
+global.location = "Nigeria"
 
 global.botname = '𝐌𝐀𝐑𝐈𝐀-𝐌𝐃' //name of the bot
 
 //sticker details
 global.stickername = '💙𓆩‎✘AʏᴜSʜ✘𓆪_✰'
 global.packname = 'Sticker By'
-global.author = 'Maria Bot'
+global.author = 'Roy'
 //console view/theme
 global.themeemoji = '🧩'
 global.wm = "Ayush botz inc."
@@ -22,7 +22,7 @@ global.wm = "Ayush botz inc."
 global.link = 'https://whatsapp.com/channel/0029VaImo5ZG3R3qjKOdyr1I'
 
 //custom prefix
-global.prefa = ['!']
+global.prefa = ['.']
 
 //false=disable and true=enable
 global.welcome = false //auto welcome
@@ -40,7 +40,7 @@ global.autoread_status = false //auto view status/story
 global.mess = {
     done: '*here you go!* \n\n*🎀 Maria Bot 🎀*\n\n*🧩 Bot link:* \nhttps://github.com/AYUSH-PANDEY023/Maria-MD\n',
     prem: '*This feature can be used by premium user only*',
-    admin: '*This feature can be used by admin only*',
+    admin: '*This feature can only be used by my Master Roy*',
     botAdmin: '*This feature can only be used when the bot is a group admin* ',
     owner: '*This feature can be used by owner only*',
     group: '*This feature is only for groups*',
